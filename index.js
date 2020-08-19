e4eda91989c3a1d7116214aa62583f3beac06aab
johnjstr <johnjstr@outlook.com>
2020-08-19 19:17:32 -0400

    final

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('my files are connected');
+window.onscroll = function() {myFunction()};
+
+let navBar = document.querySelector('nav');
+
+let sticky = navBar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navBar.classList.add ('sticky')
+        console.log('just checking');
+    } else {
+        navBar.classList.remove('sticky');
+    }
+}
